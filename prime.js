f78117bd66c05d39894e57afc51a8138cc45a190
RajaneRahul <rahulrajane177@gmail.com>
2022-11-29 23:34:14 +0530

    its done

--- prime.js
+++ prime.js
@@ -1,13 +1,13 @@
 function identifyPrime(num) {
     let count=0;
-    for(let i=0;i<=num;i++){
+    for(let i=1;i<=num;i++){
         if(num%i==0){
             count++;
         }
     }
-    if(count=0){
-        console.log("Yes")
+    if(count==2){
+        console.log("its Prime")
     }else{
-        console.log("No")
+        console.log("Not Prime")
     }
 }
